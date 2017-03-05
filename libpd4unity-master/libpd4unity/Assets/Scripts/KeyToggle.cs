85521fff92e486e4e859d48cb683f718fb925083
chrisjdarcy <chrisjdarcy@ymail.com>
2017-03-05 20:05:23 +0000

    Keyboard toggling working

---
+++ libpd4unity-master/libpd4unity/Assets/Scripts/KeyToggle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Toggle))]
+public class KeyToggle : MonoBehaviour {
+
+	public KeyCode key;
+	public Toggle toggle;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (key)) {
+			toggle.isOn = !toggle.isOn;
+		}
+		}
+}
