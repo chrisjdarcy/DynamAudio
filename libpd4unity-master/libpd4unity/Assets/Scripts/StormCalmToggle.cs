ccaee173fabb006d195a78850e1ad2a42d024841
chrisjdarcy <chrisjdarcy@ymail.com>
2017-03-05 21:17:13 +0000

    Calm Storm Toggle

---
+++ libpd4unity-master/libpd4unity/Assets/Scripts/StormCalmToggle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Toggle))]
+public class StormCalmToggle: MonoBehaviour {
+
+	public KeyCode key1;
+	public Toggle toggle1;
+	public Toggle toggle2;
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (key1)) {
+			toggle2.isOn = toggle1.isOn;
+		}
+		
+	}
+}
