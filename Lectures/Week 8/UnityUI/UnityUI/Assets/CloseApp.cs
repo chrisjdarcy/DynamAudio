ec8f0778cd802d0d75f50e0d035ff1e6ee6ccab5
chrisjdarcy <chrisjdarcy@ymail.com>
2017-05-01 07:34:59 +0100

    It werked

---
+++ Lectures/Week 8/UnityUI/UnityUI/Assets/CloseApp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using LibPDBinding;
+using UnityEngine.UI;
+
+public class CloseApp : MonoBehaviour {
+
+
+	public void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
